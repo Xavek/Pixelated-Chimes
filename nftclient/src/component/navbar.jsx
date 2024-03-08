805fedd28d07b565d8c9fbfdf220992fe67b9a2b
Xavek <theofficalgautam200@gmail.com>
2024-03-08 14:42:13 +0545

    add starknet react config

---
+++ nftclient/src/component/navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    
+    return (
+    <nav className="bg-black p-4 flex justify-between">
+      <div className="flex items-center">
+        <span className="text-white text-xl font-bold">MarketPlace</span>
+      </div>
+      <div className="flex items-center">
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- nftclient/src/main.jsx
+++ nftclient/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {mainnet, goerli, sepolia} from '@starknet-react/chains'
+import {StarknetConfig, publicProvider, argent, braavos, useInjectedConnectors, voyager} from '@starknet-react/core'
 import App from './App.jsx'
 import './index.css'
 
+const connectors = [argent(), braavos()]
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StarknetConfig connectors={connectors} provider={publicProvider()} explorer={voyager} chains={[mainnet, goerli, sepolia]}>
     <App />
-  </React.StrictMode>,
+  </StarknetConfig>
 )
