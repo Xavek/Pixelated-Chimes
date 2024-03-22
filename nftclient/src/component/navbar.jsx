c24e86c210e0c025f2fea6a578c5d14b11ce9beb
Xavek <61218841+Xavek@users.noreply.github.com>
2024-03-22 13:43:35 +0545

    UI wallet account feat (#17)
    * add wallet connect
    
    * add wallet disconnect
    
    * add wallet btn tcss

--- nftclient/src/component/navbar.jsx
+++ nftclient/src/component/navbar.jsx
@@ -1,10 +1,41 @@
+import { useAccount, useConnect, useDisconnect } from "@starknet-react/core";
+
 const Navbar = () => {
+  const { connect, connectors } = useConnect();
+  const { address, status, account } = useAccount();
+  const { disconnect } = useDisconnect();
+  const sliceAddressForView = (addrs) => {
+    return `${addrs.slice(0, 6)}...${addrs.slice(-6)}`;
+  };
   return (
     <nav className="bg-black p-4 flex justify-between">
       <div className="flex items-center">
         <span className="text-white text-xl font-bold">MarketPlace</span>
       </div>
-      <div className="flex items-center"></div>
+      <div className="flex items-center ">
+        <ul className="flex items-start">
+          {status === "disconnected" &&
+            connectors.map((connector) => (
+              <li key={connector.id}>
+                <button
+                  onClick={() => connect({ connector })}
+                  className="bg-white text-black px-6 py-2 mx-2 rounded-md"
+                >
+                  Connect To {connector.id}
+                </button>
+              </li>
+            ))}
+
+          {status === "connected" && (
+            <button
+              className="bg-white text-black px-6 py-2 mx-2 rounded-md"
+              onClick={() => disconnect()}
+            >
+              {sliceAddressForView(address)}
+            </button>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
