ffc6a94d557ce1e99db9f75411237518591cabe9
Xavek <theofficalgautam200@gmail.com>
2024-03-30 19:36:19 +0545

    add ui for nft see n buy

--- nftclient/src/App.jsx
+++ nftclient/src/App.jsx
@@ -1,10 +1,11 @@
+import ProductList from "./component/bodyBuyNFT";
 import UploadForm from "./component/bodyUploadAndMint";
 import Navbar from "./component/navbar";
 function App() {
   return (
     <>
       <Navbar />
-      <UploadForm />
+      <ProductList />
     </>
   );
 }
