aa5dd61cc015657285c35433f27c47d2364b73f8
Xavek <theofficalgautam200@gmail.com>
2024-04-03 12:49:27 +0545

    add erc20 approve api

---
+++ nftclient/src/lib/erc721Api.js
@@ -0,0 +1,12 @@
+// Write Methods
+export const doERC20Approve = async (
+  ERC721Manager,
+  account,
+  amount,
+  contractAddress,
+) => {};
+export const buyNFT = async (ERC721Manager, account, amount) => {};
+
+export const uploadNFT = async (ERC721Manager, nftName, tokenURI, amount) => {};
+
+// Read Methods
